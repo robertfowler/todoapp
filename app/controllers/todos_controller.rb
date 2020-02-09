926dabccc78b830dcd21e8f0e23ed6be628a8089
Rob Fowler <intellibuzz@gmail.com>
2020-02-09 18:25:56 +0000

    refactored flash messages and set todo from update, edit, destroy and show

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
-    
+    before_action :set_todo, only: [:show, :edit, :update, :destroy]
     def new
         @todo = Todo.new
     end
@@ -15,15 +15,12 @@ class TodosController < ApplicationController
     end
     
     def show
-        @todo = Todo.find(params[:id])
     end
     
     def edit
-        @todo =Todo.find(params[:id])
     end
     
     def update
-        @todo = Todo.find(params[:id])
         if @todo.update(todo_params)
              flash[:notice] = "Todo was updated successfully!"
              redirect_to todo_path(@todo)
@@ -37,13 +34,16 @@ class TodosController < ApplicationController
     end
     
     def destroy
-        @todo = Todo.find(params[:id])
         @todo.delete
         flash[:notice] = "Todo was successfully deleted"
         redirect_to todos_path
     end
     
     private
+    
+    def set_todo
+        @todo = Todo.find(params[:id])
+    end
     
     def todo_params
         params.require(:todo).permit(:name, :description)
